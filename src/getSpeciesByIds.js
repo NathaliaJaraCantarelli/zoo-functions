e7f4470a6fe580207c30ce0875158daea0e01b76
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-26 20:53:09 -0300

    Requisito 1 concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui.
+function getSpeciesByIds(...ids) {
+  const dados = data.species;
+  const idsInput = [...ids];
+  const animaisIds = [];
+  const animals = dados.filter((animal) => {
+    for (let index = 0; index < idsInput.length; index += 1) {
+      if (animal.id === idsInput[index]) {
+        animaisIds.push(animal);
+      }
+    }
+    return animal;
+  });
+  console.log(animals);
+  return animaisIds;
 }
 
 module.exports = getSpeciesByIds;
