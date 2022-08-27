c425d8304e7f75496cc916b2e17763c4f8180415
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-26 23:41:22 -0300

    Requisito 2 concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const dados = data.species;
+  const especies = dados.filter((especie) => especie.name === animal);
+  const animalChoice = especies[0].residents;
+  const idade = animalChoice.every((resident) => resident.age > age);
+  return idade;
 }
 
 module.exports = getAnimalsOlderThan;
