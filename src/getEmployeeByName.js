fa21e0ba758770f48cb518bdba2c05ca4d560c97
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-27 00:16:04 -0300

    Requisito 3 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const dados = data.employees;
+  const nome = {};
+  const pessoaDados = dados
+    .find((pessoa) => pessoa.firstName === employeeName || pessoa.lastName === employeeName);
+  return employeeName === undefined ? nome : pessoaDados;
 }
 
 module.exports = getEmployeeByName;
