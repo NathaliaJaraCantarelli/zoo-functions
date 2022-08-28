0e2e4d7f01868ccbc54a566727f3192ddbc291aa
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-28 14:49:53 -0300

    Requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const dados = [data.employees, data.species];
+  let animalId = '';
+  let age = 0;
+  const informationAnimalOld = [];
+  dados[0].forEach((dado) => { if (dado.id === id) { [animalId] = dado.responsibleFor; } });
+  dados[1].forEach((animal) => {
+    if (animal.id === animalId) {
+      animal.residents.forEach((resident) => {
+        if (resident.age > age) {
+          age = resident.age;
+          informationAnimalOld[0] = resident.name;
+          informationAnimalOld[1] = resident.sex;
+          informationAnimalOld[2] = resident.age;
+        }
+      });
+    }
+  });
+  return informationAnimalOld;
 }
 
 module.exports = getOldestFromFirstSpecies;
