2e0ba01da9bd937e012d409ac35c472a77ae9c70
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-27 23:48:11 -0300

    Requisito 8 concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const listCount = {
+    adult: 0,
+    senior: 0,
+    child: 0,
+  };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      listCount.child += 1;
+    } else if (entrant.age >= 50) {
+      listCount.senior += 1;
+    } else {
+      listCount.adult += 1;
+    }
+  });
+  return listCount;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let total = 0;
+  if (entrants === undefined) { return total; }
+  const array = Object.keys(entrants);
+  if (array.length === 0) { return total; }
+  const dados = data.prices;
+  const listCount = countEntrants(entrants);
+  const totalAge = {
+    adult: listCount.adult * dados.adult,
+    senior: listCount.senior * dados.senior,
+    child: listCount.child * dados.child,
+  };
+  total = totalAge.adult + totalAge.child + totalAge.senior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
