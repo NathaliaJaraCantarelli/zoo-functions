aa6c60a80dfddd7daa0dba5a496f78196d350ef3
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-29 22:04:08 -0300

    Requisito 16 concluido

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -32,4 +32,13 @@ describe('Testes da função getOpeningHours', () => {
   it('Para os argumentos Saturday e C9:00-AM deve retornar a exceção The hour should represent a number', () => {
     expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
   });
+  it('Para os argumentos Sunday e 09:c0-AM deve retornar a exceção The minutes should represent a number', () => {
+    expect(() => getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
+  });
+  it('Para os argumentos Monday e 13:00-AM deve retornar a exceção The hour must be between 0 and 12', () => {
+    expect(() => getOpeningHours('Monday', '13:00-AM')).toThrow('The hour must be between 0 and 12');
+  });
+  it('Para os argumentos Tuesday e 09:60-AM deve retornar a exceção The minutes must be between 0 and 59', () => {
+    expect(() => getOpeningHours('Tuesday', '09:60-AM')).toThrow('The minutes must be between 0 and 59');
+  });
 });
