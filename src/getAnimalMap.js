736bed1909a337eeacb9f9c9ab7b7dde7823e940
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-29 21:30:40 -0300

    Requisito 14 concluido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const includeNamesFunction = (dados, options) => {
+  const retorno = { NE: [], NW: [], SE: [], SW: [] };
+  dados.forEach((animal) => {
+    const names = [];
+    const objeto = {};
+    names[animal.name] = [];
+    animal.residents.forEach((resident) => {
+      if ((!options.sex) || (resident.sex === options.sex)) {
+        names[animal.name].push(resident.name);
+      }
+    });
+    if (options.sorted === true) { names[animal.name].sort(); }
+    objeto[animal.name] = names[animal.name];
+    retorno[animal.location].push(objeto);
+  });
+  return retorno;
+};
 
+function getAnimalMap(options) {
+  const dados = data.species;
+  let retorno = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  if ((!options) || (!options.includeNames)) {
+    dados.forEach((animal) => retorno[animal.location].push(animal.name));
+  } else if (options.includeNames === true) {
+    retorno = includeNamesFunction(dados, options);
+  }
+  return retorno;
+}
+console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
