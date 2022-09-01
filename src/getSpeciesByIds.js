2b0e04a05e8a27e082827f229d77f29e14a91896
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-31 23:34:17 -0300

    Refatorando requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,16 +3,8 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   const dados = data.species;
   const idsInput = [...ids];
-  const animaisIds = [];
-  const animals = dados.filter((animal) => {
-    for (let index = 0; index < idsInput.length; index += 1) {
-      if (animal.id === idsInput[index]) {
-        animaisIds.push(animal);
-      }
-    }
-    return animal;
-  });
-  console.log(animals);
+  const animaisIds = dados.filter((animal) => idsInput
+    .find((idInput) => idInput === animal.id));
   return animaisIds;
 }
 
