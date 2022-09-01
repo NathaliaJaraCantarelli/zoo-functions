fbc7f66a96b195db32af16d73e0a9408429d63d6
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-31 22:23:21 -0300

    Refatornando requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,20 +1,51 @@
 const data = require('../data/zoo_data');
 
-const includeNamesFunction = (dados, options) => {
-  const retorno = { NE: [], NW: [], SE: [], SW: [] };
-  dados.forEach((animal) => {
-    const names = [];
-    const objeto = {};
-    names[animal.name] = [];
-    animal.residents.forEach((resident) => {
-      if ((!options.sex) || (resident.sex === options.sex)) {
-        names[animal.name].push(resident.name);
+// const includeNamesFunction = (dados, options) => {
+//   const retorno = { NE: [], NW: [], SE: [], SW: [] };
+//   dados.forEach((animal) => {
+//     const names = [];
+//     const objeto = {};
+//     names[animal.name] = [];
+//     animal.residents.forEach((resident) => {
+//       if ((!options.sex) || (resident.sex === options.sex)) {
+//         names[animal.name].push(resident.name);
+//       }
+//     });
+//     if (options.sorted === true) { names[animal.name].sort(); }
+//     objeto[animal.name] = names[animal.name];
+//     retorno[animal.location].push(objeto);
+//   });
+//   return retorno;
+// };
+
+const criaElemento = (localizacao, options) => {
+  const dados = data.species;
+  const testeDado = dados
+    .filter((locationName) => locationName.location === localizacao).map((animal) => {
+      let objectoRetorno = {};
+      if (options.sorted) {
+        objectoRetorno = { [animal.name]: animal.residents
+          .filter((resident) => ((!options.sex) || (resident.sex === options.sex)))
+          .map((resident) => resident.name).sort(),
+        };
+      } else {
+        objectoRetorno = { [animal.name]: animal.residents
+          .filter((resident) => ((!options.sex) || (resident.sex === options.sex)))
+          .map((resident) => resident.name),
+        };
       }
+      return objectoRetorno;
     });
-    if (options.sorted === true) { names[animal.name].sort(); }
-    objeto[animal.name] = names[animal.name];
-    retorno[animal.location].push(objeto);
-  });
+  return testeDado;
+};
+
+const teste = (options) => {
+  const retorno = {
+    NE: criaElemento('NE', options),
+    NW: criaElemento('NW', options),
+    SE: criaElemento('SE', options),
+    SW: criaElemento('SW', options),
+  };
   return retorno;
 };
 
@@ -29,9 +60,9 @@ function getAnimalMap(options) {
   if ((!options) || (!options.includeNames)) {
     dados.forEach((animal) => retorno[animal.location].push(animal.name));
   } else if (options.includeNames === true) {
-    retorno = includeNamesFunction(dados, options);
+    retorno = teste(options);
   }
   return retorno;
 }
-console.log(getAnimalMap({ includeNames: true }));
+
 module.exports = getAnimalMap;
